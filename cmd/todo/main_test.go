780e1740cfdb38ec263bc4006cd34a3f791fcf37
Gabriel Bussolo <gabreeiel@gmail.com>
2022-05-28 20:34:12 +0300

    Improve -list presentation

--- cmd/todo/main_test.go
+++ cmd/todo/main_test.go
@@ -65,7 +65,7 @@ func TestTodoCLI(t *testing.T) {
 		if err != nil {
 			t.Fatal(err)
 		}
-		expected := task + "\n"
+		expected := fmt.Sprintf("  1: %s\n", task)
 
 		if expected != string(out) {
 			t.Errorf("expected %q, got %q", expected, string(out))
--- todo.go
+++ todo.go
@@ -70,3 +70,18 @@ func (l *List) Get(filename string) error {
 	}
 	return json.Unmarshal(file, l)
 }
+
+// String prints out a formatted list
+// Implements the fmt.Stringer interface
+func (l *List) String() string {
+	formatted := ""
+
+	for i, t := range *l {
+		prefix := "  "
+		if t.Done {
+			prefix = "X "
+		}
+		formatted += fmt.Sprintf("%s%d: %s\n", prefix, i+1, t.Task)
+	}
+	return formatted
+}
