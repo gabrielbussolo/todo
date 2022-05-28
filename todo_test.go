7b5d8823ce1ab354e08345f06f5971112a3d294c
Gabriel Bussolo <gabreeiel@gmail.com>
2022-05-28 13:33:23 +0300

    Add delete feature to list

--- todo_test.go
+++ todo_test.go
@@ -33,4 +33,31 @@ func TestTodoList(t *testing.T) {
 			t.Errorf("New task should be completed")
 		}
 	})
+	t.Run("delete", func(t *testing.T) {
+		l := todo.List{}
+
+		tasks := []string{
+			"Task 1",
+			"Task 2",
+			"Task 3",
+		}
+
+		for _, task := range tasks {
+			l.Add(task)
+		}
+
+		if l[0].Task != tasks[0] {
+			t.Errorf("Expected %q, got %q", tasks[0], l[0].Task)
+		}
+
+		l.Delete(2)
+
+		if len(l) != 2 {
+			t.Errorf("Expected list length %d, got %d", 2, len(l))
+		}
+
+		if l[1].Task != tasks[2] {
+			t.Errorf("Expected %q, got %q", tasks[2], l[1].Task)
+		}
+	})
 }
